87b5e3fa8268e0d92ef504d5c798ef6e182fab15
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2025-12-21 01:50:07 +0500

    Change baseUrl to root for deployment

--- book/docusaurus.config.ts
+++ book/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://physical-ai-humanoid-robotics-textb-nu.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics-Textbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
